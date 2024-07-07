b183c3ad23ea86d16517c21329d98eec0d4a2893
Ingo Sternberg <ingo.sternberg@fau.de>
2024-07-07 17:19:22 +0200

    feat: adjust login page and add validation
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>
    #230

--- libs/account/frontend/view/src/lib/components/login/login.page.ts
+++ libs/account/frontend/view/src/lib/components/login/login.page.ts
@@ -1,33 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IxModule } from '@siemens/ix-angular';
 
 @Component({
-	selector: 'lib-login',
-	standalone: true,
-	imports: [ CommonModule, FormsModule, IxModule ],
-	templateUrl: './login.page.html',
-	styleUrls: [ './login.page.scss' ],
-	encapsulation: ViewEncapsulation.None,
-	changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'lib-login',
+    standalone: true,
+    imports: [ CommonModule, FormsModule, IxModule ],
+    templateUrl: './login.page.html',
+    styleUrls: [ './login.page.scss' ],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPage {
-	username = '';
-	password = '';
+    protected email = '';
+    protected password = '';
+    protected wasValidated = false;
+    protected loginSuccess = false;
 
-	constructor(private router: Router) {}
+    constructor(
+        private router: Router
+    ) {}
 
-	onSubmit() {
-		// Mock authentication process
-		if (this.username === 'siemens' && this.password === 'siemens') {
-			// Redirect to dashboard on successful login
-			this.router.navigate([ '/facilities' ]);
-		} else {
-			// Display an error message for failed login
-			alert('Invalid username or password');
-		}
-	}
+
+    onSubmit(form: NgForm) {
+        this.wasValidated = true;
+
+        if (!form.valid) {
+            return;
+        }
+
+        this.loginSuccess = this.password === 'siemens';
+        if(this.loginSuccess){
+            this.router.navigate([ '/' ]);
+        }
+
+    }
 
 }
