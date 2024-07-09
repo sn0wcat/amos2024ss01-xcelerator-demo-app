54b7966756a3fea3ddc57729e212e95b36a430f1
Ingo Sternberg <ingo.sternberg@fau.de>
2024-07-09 16:56:44 +0200

    fix: change to fromData.selectFacility
    Signed-off-by: Ingo Sternberg <ingo.sternberg@fau.de>
    #193

--- libs/cases/frontend/view/src/lib/components/create-case/create-case.component.ts
+++ libs/cases/frontend/view/src/lib/components/create-case/create-case.component.ts
@@ -184,7 +184,7 @@ export class CreateCaseComponent implements OnInit {
      */
     private mapFormData(formData: CaseFormData) {
         return {
-            assetId: this.createCaseForm.selectFacility,
+            assetId: formData.selectFacility,
             handle: 'AA-' + faker.number.int({ min: 1000, max: 9999 }),
             dueDate: formData.dueDate,
             title: formData.title,
