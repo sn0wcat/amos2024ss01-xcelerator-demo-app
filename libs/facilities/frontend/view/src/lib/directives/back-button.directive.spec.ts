bee4d879073537a90c0d68a95c716a0a7d80a043
KonsumGandalf <d.schmidt@tu-berlin.de>
2024-07-11 13:21:46 +0200

    test: add test

--- libs/facilities/frontend/view/src/lib/directives/back-button.directive.spec.ts
+++ libs/facilities/frontend/view/src/lib/directives/back-button.directive.spec.ts
@@ -1,20 +1,42 @@
 import { Location } from '@angular/common';
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { BackButtonDirective } from './back-button.directive';
 
+@Component({
+	template: `<button backButton>Back</button>`,
+})
+class TestComponent {}
+
 describe('BackButtonDirective', () => {
-    let directive: BackButtonDirective;
+	let location: Location;
+	let fixture: any;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [ Location, BackButtonDirective ]
-        });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [TestComponent],
+			imports: [BackButtonDirective],
+			providers: [Location],
+		});
 
-        directive = TestBed.inject(BackButtonDirective);
-    });
+		fixture = TestBed.createComponent(TestComponent);
+		location = TestBed.inject(Location);
+		fixture.detectChanges();
+	});
 
-    it('should create an instance', () => {
-        expect(directive).toBeTruthy();
-    });
+	it('should create an instance', () => {
+		const directive = new BackButtonDirective(location);
+		expect(directive).toBeTruthy();
+	});
+
+	it('should call location.back() on click', () => {
+		jest.spyOn(location, 'back');
+
+		const button = fixture.debugElement.query(By.directive(BackButtonDirective));
+		button.triggerEventHandler('click', null);
+
+		expect(location.back).toHaveBeenCalled();
+	});
 });
